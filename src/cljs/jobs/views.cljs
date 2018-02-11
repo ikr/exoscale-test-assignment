55b0b591859b265c494f29e2220f1637be08f31a
Ivan Krechetov <ikr@ikr.su>
2018-02-11 17:44:52 +0100

    Outline the page layout

--- src/cljs/jobs/views.cljs
+++ src/cljs/jobs/views.cljs
@@ -1,8 +1,28 @@
 (ns jobs.views
-  (:require [re-frame.core :as re-frame]
+  (:require [re-frame.core :as r]
             [clojure.string :as str]
             [jobs.routes :refer [url-for]]
-            [jobs.subs :as subs]))
+            [jobs.subs :as subs]
+            [jobs.config :as config]))
 
-(defn main-panel []
-  [:div [:p "hello"]])
+(defn jobs-panel []
+  [:p "All jobs"])
+
+(defn current-job-panel []
+  [:p "Current job"])
+
+(defn content []
+  (let [active (r/subscribe [:active-panel])]
+    (condp = @active
+      :jobs-panel (jobs-panel)
+      :current-job-panel (current-job-panel))))
+
+(defn footer []
+  [:div.footer
+   (when config/debug?
+     [:button
+      {:type "button" :on-click #(r/dispatch [:run-tests])}
+      "Run tests"])])
+
+(defn layout []
+  [:div.container (content) (footer)])
