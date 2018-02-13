b6ba1e11c54136744d924af9b83534757c7663ba
Ivan Krechetov <ikr@ikr.su>
2018-02-13 17:27:28 +0100

    Extract view functions for the jobs list panel

---
+++ src/cljs/jobs/views-jobs.cljs
@@ -0,0 +1,50 @@
+(ns jobs.views-jobs
+  (:require [re-frame.core :as r]
+            [clojure.string :as str]))
+
+(defn jobs-table-header-row []
+  [:tr
+   [:th.text-right "#"]
+   [:th "Company"]
+   [:th "Title"]
+   [:th "Keywords"]
+   [:th ""]])
+
+(defn job-row [job]
+  [:tr {:key (str/join ["r" (:id job)])}
+   [:td.text-right (:id job)]
+   [:td (:company job)]
+   [:td (:title job)]
+   [:td (str/join " " (:keywords job))]
+   [:td.text-right
+    [:button.btn.btn-sm.btn-secondary
+     {:type "button"
+      :key "e"}
+     "Edit"]
+    " "
+    [:button.btn.btn-sm.btn-warning
+     {:type "button"
+      :key "d"}
+     "Delete"]]])
+
+(defn jobs-table []
+  (let [fetch-state (r/subscribe [:jobs-fetch])
+        jobs-list (r/subscribe [:jobs-list])]
+    (condp = @fetch-state
+      :not-asked [:div.alert.alert-info "Initializing…"]
+      :loading [:div.alert.alert-info "Fetching…"]
+      :failure [:div.alert.alert-danger "Failed fetching jobs :("]
+      :success [:table.table
+                [:thead (jobs-table-header-row)]
+                [:tbody (map job-row @jobs-list)]])))
+
+(defn jobs-panel []
+  [:div
+   [:h1 "All jobs"]
+   [:div.text-right
+    {:style {:margin "12px"}}
+    [:button.btn.btn-lg.btn-primary
+     {:type "button"
+      :on-click #(r/dispatch [:edit-new-job])}
+     "New"]]
+   (jobs-table)])
--- src/cljs/jobs/views.cljs
+++ src/cljs/jobs/views.cljs
@@ -1,56 +1,11 @@
 (ns jobs.views
   (:require [re-frame.core :as r]
             [clojure.string :as str]
+            [jobs.views-jobs :as views-jobs]
             [jobs.routes :refer [url-for]]
             [jobs.subs :as subs]
             [jobs.config :as config]))
 
-(defn jobs-table-header-row []
-  [:tr
-   [:th.text-right "#"]
-   [:th "Company"]
-   [:th "Title"]
-   [:th "Keywords"]
-   [:th ""]])
-
-(defn job-row [job]
-  [:tr {:key (str/join ["r" (:id job)])}
-   [:td.text-right (:id job)]
-   [:td (:company job)]
-   [:td (:title job)]
-   [:td (str/join " " (:keywords job))]
-   [:td.text-right
-    [:button.btn.btn-sm.btn-secondary
-     {:type "button"
-      :key "e"}
-     "Edit"]
-    " "
-    [:button.btn.btn-sm.btn-warning
-     {:type "button"
-      :key "d"}
-     "Delete"]]])
-
-(defn jobs-table []
-  (let [fetch-state (r/subscribe [:jobs-fetch])
-        jobs-list (r/subscribe [:jobs-list])]
-    (condp = @fetch-state
-      :not-asked [:div.alert.alert-info "Initializing…"]
-      :loading [:div.alert.alert-info "Fetching…"]
-      :failure [:div.alert.alert-danger "Failed fetching jobs :("]
-      :success [:table.table
-                [:thead (jobs-table-header-row)]
-                [:tbody (map job-row @jobs-list)]])))
-
-(defn jobs-panel []
-  [:div
-   [:h1 "All jobs"]
-   [:div.text-right
-    {:style {:margin "12px"}}
-    [:button.btn.btn-lg.btn-primary
-     {:type "button"
-      :on-click #(r/dispatch [:edit-new-job])}
-     "New"]]
-   (jobs-table)])
 
 (defn current-job-panel []
   [:div
@@ -66,7 +21,7 @@
 (defn content []
   (let [active (r/subscribe [:active-panel])]
     (condp = @active
-      :jobs-panel (jobs-panel)
+      :jobs-panel (views-jobs/jobs-panel)
       :current-job-panel (current-job-panel))))
 
 (defn footer []
