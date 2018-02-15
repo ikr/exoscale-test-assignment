0a8ccc69ea7b864f4e32db4d749d8fe12f033db2
Ivan Krechetov <ikr@ikr.su>
2018-02-15 20:14:23 +0100

    Finish the qs/encode

--- src/cljs/jobs/querystring.cljs
+++ src/cljs/jobs/querystring.cljs
@@ -3,5 +3,16 @@
 
 (defn e [x] (.encodeURIComponent js/window x))
 
+(defn param [name value]
+  (str/join [name "=" (e value)]))
+
+(defn encode-keywords [job]
+  (str/join "&" (map (partial param "keywords") (:keywords job))))
+
+
 (defn encode [job]
-  (str/join "=" (map #(str/join ["keywords=", (e %)]) (:keywords job))))
+  (str/join
+   "&"
+   [((comp (partial param "company") :company) job)
+    ((comp (partial param "title") :title) job)
+    (encode-keywords job)]))
--- src/cljs/jobs/test.cljs
+++ src/cljs/jobs/test.cljs
@@ -2,6 +2,11 @@
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
             [jobs.querystring :as qs]))
 
+(deftest test-qs-encode-keywords
+  (is (=
+       (qs/encode-keywords {:keywords ["x" "y"]})
+       "keywords=x&keywords=y")))
+
 (deftest test-qs-encode
   (is (=
        (qs/encode {:company "c"
